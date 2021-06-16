a1343fb4ad17937b550cf04d9043461b2790b01c
B0544218 <32070909+B0544218@users.noreply.github.com>
2021-06-16 21:36:02 +0800

    Add files via upload

---
+++ Linux_command/script/ngro.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir ~/Desktop/ngrok
+cd ~/Desktop/ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ~/Desktop/ngrok/ngrok-stable-linux-amd64.zip
+#./ngrok authtoken (token)
+#it will save to /home/kevin/.ngrok2/ngrok.yml
+
+#./ngrok http 8888
