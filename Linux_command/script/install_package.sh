e0e3817be5d638bf4b6b43fd39829d0cf796d14a
B0544218 <32070909+B0544218@users.noreply.github.com>
2021-06-23 15:23:33 +0800

    Create install_package.sh

---
+++ Linux_command/script/install_package.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# backup資料 : https://drive.google.com/drive/folders/1KppSgKpDk_YLZUymY4aU640Di7DP-4_5?usp=sharing
+## backup資料包含 keyboard、vim、pyhon、npm、docker
+### backup完後還要再安裝相關套件
+# sudo apt-get install gcin gcin-qt5-immodule gcin-anthy gcin-voice
+sudo apt-get install curl vim git 
+# sudo apt-get install exuberant-ctags ack-grep
+sudo apt-get install python3-pip
+pip3 install pep8
+# pip3 install flake8 pyflakes isort yapf
+# pip3 install neovim
+sudo apt-get install docker.io nodejs npm
+sudo apt-get install net-tools
+sudo apt-get install gparted
+
