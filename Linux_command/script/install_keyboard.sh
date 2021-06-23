8cab87491dd1ef6e7dbb78b4c973395a7179ac1e
B0544218 <32070909+B0544218@users.noreply.github.com>
2021-06-23 12:40:07 +0800

    Update install_keyboard.sh

--- Linux_command/script/install_keyboard.sh
+++ Linux_command/script/install_keyboard.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 cd ~
+# 安裝「gcin
 sudo apt-get install gcin gcin-qt5-immodule gcin-anthy gcin-voice
+# 接著執行下面指令，將系統的輸入法框架設「gcin」
 im-config -n gcin
+# 執行下面指令，確認上個步驟是否有更改
 cat ~/.xinputrc
+# 如何確認系統上安裝了那些關於「gcin」的套件
 dpkg -l | grep gcin
+# 重啟
+reboot
