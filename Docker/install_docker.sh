547abf61da31e1fe36ada6be5557964ab8850245
B0544218 <32070909+B0544218@users.noreply.github.com>
2021-06-23 16:41:52 +0800

    Update install_docker.sh

--- Docker/install_docker.sh
+++ Docker/install_docker.sh
@@ -3,7 +3,7 @@
 sudo apt-get install -y docker.io
 service docker status
 # cd /etc/ 看看group檔案中有無docker(預設應該會幫你裝)， 沒有的話執行 sudo groupadd docker (建立 docker 群組)
-sudo usermod -G docker -a (帳號) # (檢視所有系統帳號 用cat /etc/passwd 去看第一個欄位 )
+sudo usermod -G docker -a kevin # kevin為帳號 ## (檢視所有系統帳號 用cat /etc/passwd 去看第一個欄位 )
 newgrp docker # (更新docker group資訊)
 sudo systemctl restart docker # (重新啟動docker服務)
 sleep 5
