ecd674a2b35d473bce65b9a9ec96289bccb9b13c
B0544218 <32070909+B0544218@users.noreply.github.com>
2021-06-23 15:41:56 +0800

    Update install_package.sh

--- Linux_command/script/install_package.sh
+++ Linux_command/script/install_package.sh
@@ -4,13 +4,13 @@
 ## backup資料包含 keyboard、vim、pyhon、npm、docker
 ### backup完後還要再安裝相關套件
 # sudo apt-get install gcin gcin-qt5-immodule gcin-anthy gcin-voice
-sudo apt-get install curl vim git 
+sudo apt-get install -y curl vim git 
 # sudo apt-get install exuberant-ctags ack-grep
-sudo apt-get install python3-pip
+sudo apt-get install -y python3-pip
 pip3 install pep8
 # pip3 install flake8 pyflakes isort yapf
 # pip3 install neovim
-sudo apt-get install docker.io nodejs npm
-sudo apt-get install net-tools
-sudo apt-get install gparted
+sudo apt-get install -y docker.io nodejs npm
+sudo apt-get install -y net-tools
+sudo apt-get install -y gparted
 
